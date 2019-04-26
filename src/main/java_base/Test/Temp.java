ead4c5b6236d2488ccfb87b66b7a1e092fbb92a8
LDLearner <ld_abc@163.com>
2019-04-26 18:28:44 +0800

    更新代码

--- src/main/java_base/Test/Temp.java
+++ src/main/java_base/Test/Temp.java
@@ -3,20 +3,10 @@ package Test;
 /**
  * Created by LD on 2019/3/29.
  */
+@org.testng.annotations.Test
 public class Temp {
-    public static void main(String[] args) {
-        readArray(createArray(100));
+    public void Case01() {
+        System.out.println("Hi");
     }
-    static int[] createArray(int n){
-        int[] a=new int[n];
-        for (int i=0;i<n;i++){
-            a[i]=i;
-        }
-        return a;
-    }
-    static void readArray(int[] array){
-        for (int i=0;i<array.length;i++){
-            System.out.println("Arr["+i+"]="+array[i]);
-        }
-    }
+
 }
---
+++ src/test/java/Test/TempTest.java
@@ -0,0 +1,22 @@
+package Test;
+
+/**
+ * Created by LD on 2019/4/26.
+ */
+public class TempTest {
+
+    @org.testng.annotations.BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.testng.annotations.Test
+    public void testCase01() throws Exception {
+
+    }
+}
